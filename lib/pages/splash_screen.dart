dd2ffb102b23beb62b2f9c4e5503468cfb09cd90
Kerry Mukoya <175574325+Ivankerry@users.noreply.github.com>
2025-05-12 20:29:19 +0300

    Update splash_screen.dart

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -11,7 +11,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const OnboardingScreen()),
