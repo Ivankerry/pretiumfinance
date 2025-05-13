3bd2cc64f902fcde46ee046dc8167632b95564e7
Ivankerry <175574325+Ivankerry@users.noreply.github.com>
2025-05-13 12:08:25 +0300

    Code changes

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart'; // Add this import
+import 'pages/login_page.dart';
 
 // Main onboarding page widget
 class OnboardingScreen extends StatefulWidget {
